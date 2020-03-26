47d2274c554f8411d8390d5b789d72e49590827c
Abdi-Adan <53440762+Abdi-Adan@users.noreply.github.com>
2020-03-26 10:48:59 +0300

    model queries

--- src/core/views.py
+++ src/core/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+# Query the required objects
+# Set them up into the Context
+# Pass them into the templates section
+
 
 def index(request):
     return render(request, 'index.html', {})
